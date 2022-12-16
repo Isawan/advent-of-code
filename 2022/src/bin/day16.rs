a6dd759a06fc9a64afa559961fb60cb8640089b1
Isawan Millican <admin@isawan.net>
2022-12-16 16:20:57 +0000

    example question seems to work, but notthe real thing

--- 2022/src/bin/day16.rs
+++ 2022/src/bin/day16.rs
@@ -1,8 +1,8 @@
 use lazy_static::lazy_static;
 use regex::{Regex, RegexBuilder};
-use std::cmp::{max, min};
-use std::collections::BTreeSet;
-use std::collections::{BTreeMap, HashMap};
+use std::cmp::{max, min, Reverse};
+use std::collections::{BTreeMap, HashMap, VecDeque};
+use std::collections::{BTreeSet, BinaryHeap};
 use std::time::Instant;
 use structopt::StructOpt;
 
@@ -47,49 +47,239 @@ fn parse<'a>(input: &'a str) -> BTreeMap<&'a str, Valve> {
         .collect()
 }
 
-fn search(
+type PreviousPositions<'a> = BTreeSet<&'a str>;
+type NonZeroPositions<'a> = BTreeSet<&'a str>;
+type CurrentPosition<'a> = &'a str;
+type RemainingTime = u32;
+type TotalFlowRate = u32;
+type Pressure = u32;
+
+/// The best possible case is to encounter the top sorted flow rates
+/// in a row, taking 2 turns each time. This means the sum of the
+/// top (remaining_time/2) elements plus the full pressure of all the
+/// valves after that
+fn maximum_obtainable_pressure<'a>(
     map: &BTreeMap<&str, Valve>,
-    previous_positions: &BTreeSet<&str>,
-    current_position: &str,
+    visited: &BTreeSet<&str>,
     remaining_time: u32,
+    full_pressure: u32,
 ) -> u32 {
-    let valve = map.get(current_position).unwrap();
-    let mut new_previous_positions = previous_positions.clone();
-    new_previous_positions.insert(current_position);
-    let mut best_flow_rate = 0;
-
-    if remaining_time == 0 {
-        return 0;
+    let mut remaining_flows: Vec<u32> = map
+        .iter()
+        .filter(|(k, v)| !visited.contains(*k))
+        .map(|(k, v)| v.flow_rate)
+        .collect();
+    remaining_flows.sort_by(|a, b| b.cmp(&a));
+    let minimum_minutes = ((remaining_time + 1) / 2);
+    let mut building_pressure = 0;
+    let mut tmp = 0;
+    for r in remaining_flows.iter().take(minimum_minutes as usize) {
+        tmp = tmp + r;
+        building_pressure = building_pressure + tmp;
     }
+    full_pressure * (remaining_time - minimum_minutes) + building_pressure
+}
 
-    for next_position in valve.tunnels.iter() {
-        let flow_without_local_release = search(
-            map,
-            &new_previous_positions,
-            next_position,
-            remaining_time - 1,
-        );
-        let local_flow_rate = if !previous_positions.contains(current_position) {
-            valve.flow_rate
-        } else {
-            0
-        };
-        let flow_with_local_release = match remaining_time {
-            t if t == 1 => local_flow_rate,
-            t if t > 1 => {
-                local_flow_rate + search(map, &new_previous_positions, next_position, t - 2)
+//fn heap_search(map: &BTreeMap<&str, Valve>, current_position: &str, time: u32) -> Pressure {
+//    let mut best_pressure = 0;
+//    let mut heap: BinaryHeap<(
+//        Pressure,
+//        TotalFlowRate,
+//        Reverse<RemainingTime>,
+//        PreviousPositions,
+//        CurrentPosition,
+//        Vec<&str>,
+//    )> = BinaryHeap::new();
+//    let full_pressure: u32 = map.values().map(|x| x.flow_rate).sum();
+//    heap.push((
+//        0,
+//        0,
+//        Reverse(time),
+//        BTreeSet::new(),
+//        current_position,
+//        vec![current_position],
+//    ));
+//    loop {
+//        if let Some((pressure, total_flow_rate, Reverse(remaining_time), prev, curr, path)) =
+//            heap.pop()
+//        {
+//            let valve = map.get(curr).unwrap();
+//            let mut new_prev = prev.clone();
+//            new_prev.insert(curr);
+//            //println!(
+//            //    "path:{:?}, pressure: {:?} , totol_flow_rate:{:?}, remaining: {:?}, prev: {:?}, cur: {:?}",
+//            //    path, pressure, total_flow_rate, remaining_time, prev, curr
+//            //);
+//
+//            // let remaining_pressure = maximum_obtainable_pressure(map, &prev, remaining_time, full_pressure);
+//            //
+//            // skip if impossible to get best
+//            //if remaining_pressure < best_pressure {
+//            //    continue;
+//            //}
+//
+//            // out of time
+//            if remaining_time == 0 {
+//                best_pressure = max(best_pressure, pressure);
+//                continue;
+//            }
+//
+//            // if all valves have been openned, its straightforward to determine total pressure
+//            // without simulation
+//            if map.keys().filter(|x| !prev.contains(*x)).count() == 0 {
+//                best_pressure = max(best_pressure, pressure + full_pressure * remaining_time)
+//            }
+//
+//            for next_position in valve.tunnels.iter() {
+//                let mut next_path = path.clone();
+//                next_path.push(&next_position);
+//
+//                // search local_flow without release
+//                heap.push((
+//                    pressure + total_flow_rate,
+//                    total_flow_rate,
+//                    Reverse(remaining_time - 1),
+//                    new_prev.clone(),
+//                    next_position,
+//                    next_path.clone(),
+//                ));
+//
+//                let local_flow_rate = if !prev.contains(curr) {
+//                    valve.flow_rate
+//                } else {
+//                    0
+//                };
+//                let next_flow_rate = total_flow_rate + local_flow_rate;
+//
+//                if remaining_time == 1 {
+//                    best_pressure = max(best_pressure, pressure + next_flow_rate);
+//                } else if remaining_time > 1 {
+//                    heap.push((
+//                        pressure + total_flow_rate + next_flow_rate,
+//                        next_flow_rate,
+//                        Reverse(remaining_time - 2),
+//                        new_prev.clone(),
+//                        next_position,
+//                        next_path.clone(),
+//                    ));
+//                } else {
+//                    panic!("impossible condition");
+//                }
+//            }
+//        } else {
+//            break;
+//        }
+//    }
+//    best_pressure
+//}
+
+fn distance(map: &BTreeMap<&str, Valve>, from: &str, to: &str) -> Option<u32> {
+    let mut heap = BinaryHeap::new();
+    let mut visited = BTreeSet::new();
+    heap.push((0, from));
+    loop {
+        if let (dist, position) = heap.pop()? {
+            if position == to {
+                return Some(dist);
             }
-            _ => panic!("expected this case to be dealt with higher up"),
-        };
-        // NOTE: we don't need to keep track of if the previous positions has been opened.
-        // This is because it can only be openned once, so we assume the first visit
-        // may have opened it.
-        best_flow_rate = max(
-            max(flow_without_local_release, flow_with_local_release),
-            best_flow_rate,
-        );
+            for next_position in map.get(position).unwrap().tunnels.iter() {
+                if visited.contains(next_position) {
+                    continue;
+                }
+                visited.insert(next_position);
+                heap.push((dist + 1, next_position))
+            }
+        }
     }
-    best_flow_rate
+}
+
+fn search(map: &BTreeMap<&str, Valve>, current_position: &str, time: u32) -> Pressure {
+    let mut queue: VecDeque<(
+        Pressure,
+        TotalFlowRate,
+        RemainingTime,
+        NonZeroPositions,
+        CurrentPosition,
+        Vec<&str>,
+    )> = VecDeque::new();
+
+    let mut nonzero_positions = BTreeSet::new();
+    let mut best_pressure = 0;
+    let full_pressure: u32 = map.values().map(|x| x.flow_rate).sum();
+    nonzero_positions.extend(
+        map.iter()
+            .filter_map(|(k, v)| if v.flow_rate != 0 { Some(k) } else { None }),
+    );
+    queue.push_back((0, 0, time, nonzero_positions, current_position, vec![]));
+    loop {
+        if let Some((pressure, total_flow_rate, remaining_time, non_zeros, curr, path)) =
+            queue.pop_front()
+        {
+            // out of time
+            if remaining_time == 0 {
+                best_pressure = max(best_pressure, pressure);
+                continue;
+            }
+
+            // we've openned everything, just watch
+            if non_zeros.len() == 0 {
+                best_pressure = max(
+                    best_pressure,
+                    pressure + total_flow_rate * (remaining_time - 1),
+                );
+                continue;
+            }
+
+            // // spend one minute openning then call self with zero time to clean up
+            // if remaining_time == 1 {
+            //     let mut new_non_zeros = non_zeros.clone();
+            //     let c = new_non_zeros.remove(curr);
+            //     queue.push_back((
+            //         pressure + total_flow_rate + valve.flow_rate,
+            //         total_flow_rate + valve.flow_rate,
+            //         0,
+            //         new_non_zeros,
+            //         curr,
+            //         path.clone(),
+            //     ));
+            //     continue;
+            // }
+
+            // leaving valve location down a tunnel
+            // start by finding candidate locations
+            for next_position in non_zeros.iter() {
+                let next_valve = map.get(next_position).unwrap();
+                let travel_time = distance(map, curr, next_position).unwrap();
+
+                let mut next_path = path.clone();
+                next_path.push(&next_position);
+
+                let mut new_non_zeros = non_zeros.clone();
+                new_non_zeros.remove(next_position);
+
+                // don't travel if we're not going to get to the destination.
+                // Just calculate remaining total pressure and stop this branch
+                if remaining_time <= travel_time {
+                    best_pressure = max(best_pressure, pressure + total_flow_rate * remaining_time);
+                    continue;
+                }
+
+                queue.push_back((
+                    pressure
+                        + (total_flow_rate + next_valve.flow_rate)
+                        + ((total_flow_rate) * travel_time),
+                    total_flow_rate + next_valve.flow_rate,
+                    remaining_time - 1 - travel_time, // time travelling minus one to open the valve
+                    new_non_zeros,
+                    next_position,
+                    next_path,
+                ));
+            }
+        } else {
+            break;
+        }
+    }
+    best_pressure
 }
 
 fn main() {
@@ -97,7 +287,8 @@ fn main() {
     let args = Cli::from_args();
     let input = std::fs::read_to_string(args.path.as_path()).unwrap();
     let map = parse(&input);
-    let cost = search(&map, &BTreeSet::new(), "AA", 30);
+    println!("{}", map.values().map(|x| x.flow_rate).sum::<u32>());
+    println!("solution 1: {}", search(&map, "AA", 30));
     println!("time: {}", start_time.elapsed().as_micros());
 }
 
@@ -112,12 +303,28 @@ mod tests {
     }
 
     #[test]
-    fn test_search() {
+    fn test_heap_small_search() {
         let input = include_str!("../../input/day16-test");
         let map = parse(input);
-        assert_eq!(search(&map, &BTreeSet::new(), "AA", 1), 0);
-        assert_eq!(search(&map, &BTreeSet::new(), "AA", 2), 20);
-        assert_eq!(search(&map, &BTreeSet::new(), "AA", 3), 21);
-        assert_eq!(search(&map, &BTreeSet::new(), "AA", 4), 23);
+        assert_eq!(search(&map, "AA", 1), 0);
+        assert_eq!(search(&map, "AA", 2), 20);
+        assert_eq!(search(&map, "AA", 3), 40);
+    }
+
+    #[test]
+    fn test_distances() {
+        let input = include_str!("../../input/day16-test");
+        let map = parse(input);
+        assert_eq!(distance(&map, "AA", "DD"), Some(1));
+        assert_eq!(distance(&map, "AA", "II"), Some(1));
+        assert_eq!(distance(&map, "AA", "BB"), Some(1));
+        assert_eq!(distance(&map, "AA", "CC"), Some(2));
+    }
+
+    #[test]
+    fn test_heap_big_search() {
+        let input = include_str!("../../input/day16-test");
+        let map = parse(input);
+        assert_eq!(search(&map, "AA", 30), 1651);
     }
 }
