ab898b6175c98546e493451379a3a3933f52b97c
Isawan Millican <admin@isawan.net>
2023-01-10 02:43:08 +0000

    Almost there, the ending needs fixing up

--- 2022/src/bin/day16.rs
+++ 2022/src/bin/day16.rs
@@ -52,15 +52,17 @@ fn parse<'a>(input: &'a str) -> BTreeMap<&'a str, Valve> {
 
 #[derive(Debug, Clone, PartialEq, Eq, Ord)]
 struct Agent<'a> {
+    id: u32,
     destination: &'a str,
     arrival_at_remaining_time: u32,
 }
 
 impl Agent<'_> {
-    fn new() -> Self {
+    fn new(id: u32, time: u32) -> Self {
         Agent {
+            id,
             destination: "AA",
-            arrival_at_remaining_time: 0,
+            arrival_at_remaining_time: time,
         }
     }
 }
@@ -69,7 +71,7 @@ impl PartialOrd for Agent<'_> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(
             self.arrival_at_remaining_time
-                .cmp(&self.arrival_at_remaining_time),
+                .cmp(&other.arrival_at_remaining_time),
         )
     }
 }
@@ -81,6 +83,7 @@ struct State<'a> {
     remaining_time: u32,
     to_visit: BTreeSet<&'a str>,
     agents: BinaryHeap<Agent<'a>>,
+    trace: Vec<Tracepoint<'a>>,
 }
 
 impl PartialOrd for State<'_> {
@@ -102,6 +105,27 @@ impl PartialEq for State<'_> {
 }
 
 impl Eq for State<'_> {}
+
+#[derive(Debug, Clone)]
+struct Tracepoint<'a> {
+    pressure: u32,
+    total_flow_rate: u32,
+    remaining_time: u32,
+    to_visit: BTreeSet<&'a str>,
+    agents: BinaryHeap<Agent<'a>>,
+}
+
+impl<'a> Tracepoint<'a> {
+    fn new(state: &State<'a>) -> Self {
+        Self {
+            pressure: state.pressure,
+            total_flow_rate: state.total_flow_rate,
+            remaining_time: state.remaining_time,
+            to_visit: state.to_visit.clone(),
+            agents: state.agents.clone(),
+        }
+    }
+}
 
 fn distance<'a>(
     map: &BTreeMap<&'a str, Valve<'a>>,
@@ -137,7 +161,7 @@ fn distance_cache<'a, 'b: 'a>(
     result
 }
 
-fn search(map: &BTreeMap<&str, Valve>, agents: u32, time: u32) -> u32 {
+fn search(map: &BTreeMap<&str, Valve>, agent_count: u32, time: u32) -> u32 {
     let mut queue = BinaryHeap::new();
 
     let mut to_visit = BTreeSet::new();
@@ -148,87 +172,108 @@ fn search(map: &BTreeMap<&str, Valve>, agents: u32, time: u32) -> u32 {
             .filter_map(|(k, v)| if v.flow_rate != 0 { Some(k) } else { None }),
     );
     let mut agents = BinaryHeap::new();
-    agents.push(Agent::new());
-
-    queue.push(State {
+    for i in 0..agent_count {
+        agents.push(Agent::new(i, time));
+    }
+    let mut init_state = State {
         pressure: 0,
         total_flow_rate: 0,
         remaining_time: time,
         to_visit,
         agents,
-    });
+        trace: vec![],
+    };
+    init_state.trace.push(Tracepoint::new(&init_state));
+    queue.push(init_state);
     let distance_cache = distance_cache(map);
+    let mut count = 0;
+    let mut best_trace = vec![];
 
     while let Some(state) = queue.pop() {
-        let State {
-            pressure,
-            total_flow_rate,
-            remaining_time,
-            to_visit,
-            agents,
-        } = state;
+        count += 1;
         // out of time
-        if remaining_time == 0 {
-            best_pressure = max(best_pressure, pressure);
+        if state.remaining_time == 0 {
+            let old_best = best_pressure;
+            best_pressure = max(best_pressure, state.pressure);
+            if old_best != best_pressure {
+                println!("first");
+                best_trace = state.trace.clone();
+            }
             continue;
         }
 
-        // we've opened everything, just watch
-        if to_visit.len() == 0 {
-            assert_eq!(full_pressure, total_flow_rate);
-            best_pressure = max(best_pressure, pressure + total_flow_rate * remaining_time);
+        // we're about to clear the valves, calculate remaining agents then wait
+        if state.to_visit.len() == 0 {
+            //assert_eq!(full_pressure, state.total_flow_rate);
+            let old_best = best_pressure;
+            let total_pressure = state.pressure + state.total_flow_rate * state.remaining_time;
+            best_pressure = max(best_pressure, total_pressure);
+            if old_best != best_pressure {
+                println!("second");
+                best_trace = state.trace.clone();
+            }
             continue;
         }
 
         // remove impossible cases
         // overestimate by assuming max pressure for the remaining time.
-        if pressure + full_pressure * remaining_time < best_pressure {
+        if state.pressure + full_pressure * state.remaining_time < best_pressure {
             continue;
         }
 
         // leaving valve location down a tunnel
         // start by finding candidate locations
-        for next_position in to_visit.iter() {
-            let mut next_agents = agents.clone();
+        for next_position in state.to_visit.iter() {
+            let mut next_agents = state.agents.clone();
             let Agent {
-                destination: curr, ..
+                id,
+                destination: curr,
+                ..
             } = next_agents.pop().unwrap();
 
-            let next_valve = map.get(next_position).unwrap();
-            let travel_time = *distance_cache.get(&(curr, next_position.clone())).unwrap();
+            let travel_time = *distance_cache.get(&(curr, next_position)).unwrap();
 
-            let mut new_to_visit = to_visit.clone();
-            new_to_visit.remove(next_position);
+            let mut next_to_visit = state.to_visit.clone();
+            next_to_visit.remove(next_position);
 
             // Don't travel if we're not going to get to the destination.
-            // If no other agent is active,
-            // just calculate remaining total pressure and remove agent from active set
-            if next_agents.len() == 0 && remaining_time <= travel_time {
-                best_pressure = max(best_pressure, pressure + total_flow_rate * remaining_time);
-                continue;
-            } else {
+            // If no other agent is active, wait here until the end
+            if state.remaining_time > travel_time {
                 next_agents.push(Agent {
+                    id,
                     destination: next_position,
-                    arrival_at_remaining_time: remaining_time - 1 - travel_time, // time travelling minus one to open the valve
+                    // time travelling minus one to open the valve
+                    arrival_at_remaining_time: state.remaining_time - 1 - travel_time,
                 })
             }
 
-            let next_pressure = pressure + (total_flow_rate) + ((total_flow_rate) * travel_time);
+            // Some special handling coming up, we need to figure out what happens between
+            // now and the next agent to open _any_ valve.
 
             // grab minimum which is the next time an agent reaches the destination
-            let remaining_time = next_agents
-                .iter()
-                .fold(u32::MAX, |x, agent| min(x, agent.arrival_at_remaining_time));
+            let next_updatable_agent = next_agents.peek().unwrap();
+            let next_valve_updated = map.get(next_updatable_agent.destination).unwrap();
 
-            queue.push(State {
+            // landing time is the minimum time until next update.
+            let landing_time =
+                state.remaining_time - next_updatable_agent.arrival_at_remaining_time;
+            let next_pressure = state.pressure + state.total_flow_rate * landing_time;
+
+            let mut next_state = State {
                 pressure: next_pressure,
-                total_flow_rate: total_flow_rate + next_valve.flow_rate,
-                remaining_time,
-                to_visit: new_to_visit,
+                total_flow_rate: state.total_flow_rate + next_valve_updated.flow_rate,
+                remaining_time: next_updatable_agent.arrival_at_remaining_time,
+                to_visit: next_to_visit,
                 agents: next_agents,
-            });
+                trace: state.trace.clone(),
+            };
+            next_state.trace.push(Tracepoint::new(&next_state));
+
+            queue.push(next_state);
         }
     }
+    println!("count: {}", count);
+    println!("trace: {:?}", best_trace);
     best_pressure
 }
 
@@ -237,7 +282,8 @@ fn main() {
     let args = Cli::from_args();
     let input = std::fs::read_to_string(args.path.as_path()).unwrap();
     let map = parse(&input);
-    println!("solution 1: {}", search(&map, 1, args.minutes));
+    //println!("solution 1: {}", search(&map, 1, args.minutes));
+    println!("solution 2: {}", search(&map, 2, 26));
     println!("time: {}", start_time.elapsed().as_micros());
 }
 
