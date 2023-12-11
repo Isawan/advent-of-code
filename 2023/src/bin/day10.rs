038a657cb40571b02cd5fcf0c107a6225a4bb0e7
Isawan Millican <admin@isawan.net>
2023-12-11 00:07:46 +0000

    Completed day 10.
    All caught up!

---
+++ 2023/src/bin/day10.rs
@@ -0,0 +1,277 @@
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::iter::Iterator;
+
+use clap::Parser;
+use nom::multi::count;
+
+#[derive(Parser, Debug, Clone)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(long, short)]
+    path: std::path::PathBuf,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum NonStartedPipe {
+    Start,
+    Pipe(Vec<(i32, i32)>),
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum ScanState {
+    Outside,
+    Inside,
+    UpOut,
+    UpIn,
+    DownOut,
+    DownIn,
+}
+
+impl ScanState {
+    pub(crate) fn is_inside(&self) -> bool {
+        matches!(self, ScanState::Inside)
+    }
+
+    pub(crate) fn next(&self, c: char) -> Self {
+        match (self, c) {
+            (ScanState::Outside, '|') => ScanState::Inside,
+            (ScanState::Outside, 'F') => ScanState::DownOut,
+            (ScanState::Outside, 'L') => ScanState::UpOut,
+            (ScanState::Inside, '|') => ScanState::Outside,
+            (ScanState::Inside, 'F') => ScanState::DownIn,
+            (ScanState::Inside, 'L') => ScanState::UpIn,
+            (ScanState::DownOut, '7') => ScanState::Outside,
+            (ScanState::DownOut, 'J') => ScanState::Inside,
+            (ScanState::DownIn, '7') => ScanState::Inside,
+            (ScanState::DownIn, 'J') => ScanState::Outside,
+            (ScanState::UpOut, '7') => ScanState::Inside,
+            (ScanState::UpOut, 'J') => ScanState::Outside,
+            (ScanState::UpIn, '7') => ScanState::Outside,
+            (ScanState::UpIn, 'J') => ScanState::Inside,
+            (s, '-') => *s,
+            _ => panic!("Invalid state transition: {:?} {}", self, c),
+        }
+    }
+}
+
+fn pipe(tile: char) -> NonStartedPipe {
+    match tile {
+        '|' => NonStartedPipe::Pipe(vec![(0, 1), (0, -1)]),
+        '-' => NonStartedPipe::Pipe(vec![(-1, 0), (1, 0)]),
+        'L' => NonStartedPipe::Pipe(vec![(1, 0), (0, -1)]),
+        'J' => NonStartedPipe::Pipe(vec![(-1, 0), (0, -1)]),
+        '7' => NonStartedPipe::Pipe(vec![(0, 1), (-1, 0)]),
+        'F' => NonStartedPipe::Pipe(vec![(0, 1), (1, 0)]),
+        '.' => NonStartedPipe::Pipe(vec![]),
+        'S' => NonStartedPipe::Start,
+        _ => panic!("Invalid tile: {}", tile),
+    }
+}
+
+fn is_vertical(((pipe_x, pipe_y), adjacent): (&(i32, i32), &Vec<(i32, i32)>)) -> bool {
+    adjacent.iter().any(|(_, y)| *y - pipe_y != 0)
+}
+
+fn is_horizontal(((pipe_x, pipe_y), adjacent): (&(i32, i32), &Vec<(i32, i32)>)) -> bool {
+    adjacent.iter().any(|(x, _)| *x - pipe_x != 0)
+}
+
+fn resolve_start(
+    mut pipes: HashMap<(i32, i32), Vec<(i32, i32)>>,
+    start: (i32, i32),
+) -> HashMap<(i32, i32), Vec<(i32, i32)>> {
+    let mut start_adjacent_pipes: Vec<(i32, i32)> = pipes
+        .iter()
+        .filter_map(|(k, v)| if v.contains(&start) { Some(k) } else { None })
+        .cloned()
+        .collect();
+    for pipe in start_adjacent_pipes.iter() {
+        pipes.entry(start).or_default().push(*pipe);
+    }
+    pipes
+}
+
+fn build_pipe_map(input: &str) -> ((i32, i32), HashMap<(i32, i32), Vec<(i32, i32)>>) {
+    let mut pipes: HashMap<(i32, i32), Vec<(i32, i32)>> = HashMap::new();
+    let mut start = None;
+    for (y, line) in input.lines().enumerate() {
+        for (x, tile) in line.chars().enumerate() {
+            let pipe = pipe(tile);
+            match pipe {
+                NonStartedPipe::Start => {
+                    start = Some((x as i32, y as i32));
+                }
+                NonStartedPipe::Pipe(adjacent) => {
+                    if !adjacent.is_empty() {
+                        pipes.entry((x as i32, y as i32)).or_default().extend(
+                            adjacent
+                                .iter()
+                                .map(|(dx, dy)| ((x as i32) + dx, (y as i32) + dy)),
+                        );
+                    }
+                }
+            }
+        }
+    }
+    let start = start.expect("No start found");
+    (start, resolve_start(pipes, start))
+}
+
+fn find_furthest_point(map: HashMap<(i32, i32), Vec<(i32, i32)>>, start: (i32, i32)) -> u32 {
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+    let mut explore = VecDeque::new();
+    let mut max_steps = 0;
+    explore.push_back((0, start));
+    while let Some((steps, current)) = explore.pop_front() {
+        let adjacent = map.get(&current).unwrap();
+        visited.insert(current);
+        explore.extend(
+            adjacent
+                .iter()
+                .filter(|adjacent| !visited.contains(adjacent))
+                .map(|adjacent| (steps + 1, *adjacent)),
+        );
+        max_steps = max_steps.max(steps);
+    }
+    max_steps
+}
+
+fn count_enclosed_area_by_loop(
+    map: HashMap<(i32, i32), Vec<(i32, i32)>>,
+    start: (i32, i32),
+) -> u32 {
+    // prune map to only include pipe attached to start
+    let mut new_map = HashMap::new();
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+    let mut explore = VecDeque::new();
+
+    // get max y and x
+    let max_y = *map.keys().map(|(_, y)| y).max().unwrap();
+    let max_x = *map.keys().map(|(x, _)| x).max().unwrap();
+
+    explore.push_back(start);
+    while let Some(current) = explore.pop_front() {
+        let adjacent = map.get(&current).unwrap();
+        visited.insert(current);
+        explore.extend(
+            adjacent
+                .iter()
+                .filter(|adjacent| !visited.contains(adjacent))
+                .cloned(),
+        );
+        new_map.insert(current, adjacent.clone());
+    }
+
+    let map = new_map;
+
+    let mut inside = HashSet::new();
+    for j in 0..=max_y {
+        let mut ray_state = ScanState::Outside;
+        for i in 0..=max_x {
+            if let Some(kv @ (pos, adjacent)) = map.get_key_value(&(i, j)) {
+                ray_state = ray_state.next(pipe_to_char(kv));
+            } else if ray_state.is_inside() {
+                inside.insert((i, j));
+            }
+        }
+    }
+    inside.len() as u32
+}
+
+fn pipe_to_char(pipe: (&(i32, i32), &Vec<(i32, i32)>)) -> char {
+    let ((pipe_x, pipe_y), adjacent) = pipe;
+    let delta = adjacent
+        .iter()
+        .map(|(x, y)| (x - pipe_x, y - pipe_y))
+        .collect::<Vec<_>>();
+
+    match delta.as_slice() {
+        [(0, 1), (0, -1)] | [(0, -1), (0, 1)] => '|',
+        [(-1, 0), (1, 0)] | [(1, 0), (-1, 0)] => '-',
+        [(1, 0), (0, -1)] | [(0, -1), (1, 0)] => 'L',
+        [(-1, 0), (0, -1)] | [(0, -1), (-1, 0)] => 'J',
+        [(0, 1), (-1, 0)] | [(-1, 0), (0, 1)] => '7',
+        [(0, 1), (1, 0)] | [(1, 0), (0, 1)] => 'F',
+        _ => panic!("Invalid pipe: {:?}", delta),
+    }
+}
+
+fn main() {
+    let args = Cli::parse();
+    let input = std::fs::read_to_string(args.path).unwrap();
+    let (start, map) = build_pipe_map(&input);
+    println!("Part 1: {}", find_furthest_point(map.clone(), start));
+    println!(
+        "Part 2: {}",
+        count_enclosed_area_by_loop(map.clone(), start)
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_furthest_point_example_1() {
+        let input = include_str!("../../input/day10-example-1");
+        let (start, map) = build_pipe_map(input);
+        assert_eq!(find_furthest_point(map, start), 4);
+    }
+
+    #[test]
+    fn test_furthest_point_example_2() {
+        let input = include_str!("../../input/day10-example-2");
+        let (start, map) = build_pipe_map(input);
+        assert_eq!(find_furthest_point(map, start), 8);
+    }
+
+    #[test]
+    fn test_enclosed_area_example_1() {
+        let input = include_str!("../../input/day10-example-1");
+        let (start, map) = build_pipe_map(input);
+        assert_eq!(count_enclosed_area_by_loop(map, start), 1);
+    }
+
+    #[test]
+    fn test_enclosed_area_example_3() {
+        let input = include_str!("../../input/day10-example-3");
+        let (start, map) = build_pipe_map(input);
+        assert_eq!(count_enclosed_area_by_loop(map, start), 4);
+    }
+
+    #[test]
+    fn test_enclosed_area_example_4() {
+        let input = include_str!("../../input/day10-example-4");
+        let (start, map) = build_pipe_map(input);
+        assert_eq!(count_enclosed_area_by_loop(map, start), 8);
+    }
+
+    fn pipe_helper(c: char) -> ((i32, i32), Vec<(i32, i32)>) {
+        if let NonStartedPipe::Pipe(adjacent) = pipe(c) {
+            ((0, 0), adjacent)
+        } else {
+            panic!("Not a pipe");
+        }
+    }
+
+    #[test]
+    fn test_vertical_pipes() {
+        let (p, a) = pipe_helper('|');
+        assert_eq!(is_vertical((&p, &a)), true);
+
+        let (p, a) = pipe_helper('-');
+        assert_eq!(is_vertical((&p, &a)), false);
+
+        let (p, a) = pipe_helper('L');
+        assert_eq!(is_vertical((&p, &a)), true);
+
+        let (p, a) = pipe_helper('J');
+        assert_eq!(is_vertical((&p, &a)), true);
+
+        let (p, a) = pipe_helper('7');
+        assert_eq!(is_vertical((&p, &a)), true);
+
+        let (p, a) = pipe_helper('F');
+        assert_eq!(is_vertical((&p, &a)), true);
+    }
+}
---
+++ 2023/src/bin/day9.rs
@@ -0,0 +1,115 @@
+use clap::Parser;
+use nom::{
+    character::complete::{multispace1, newline, space0, space1},
+    combinator::opt,
+    multi::many1,
+    sequence::terminated,
+    IResult,
+};
+
+#[derive(Parser, Debug, Clone)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(long, short)]
+    path: std::path::PathBuf,
+}
+
+fn sequence(input: &str) -> IResult<&str, Vec<i32>> {
+    many1(terminated(nom::character::complete::i32, space0))(input)
+}
+
+fn sequences(input: &str) -> IResult<&str, Vec<Vec<i32>>> {
+    many1(terminated(sequence, opt(newline)))(input)
+}
+
+fn lower_increment(input: &[i32]) -> i32 {
+    assert_ne!(input.len(), 0);
+    if input.iter().all(|d| *d == 0) {
+        0
+    } else {
+        let next: Vec<_> = input.windows(2).map(|w| w[1] - w[0]).collect();
+        input.last().unwrap() + lower_increment(&next)
+    }
+}
+
+fn front_lower_increment(input: &[i32]) -> i32 {
+    if input.iter().all(|d| *d == 0) {
+        0
+    } else {
+        let next: Vec<_> = input.windows(2).map(|w| w[1] - w[0]).collect();
+        input.first().unwrap() - front_lower_increment(&next)
+    }
+}
+
+fn part1(input: &[Vec<i32>]) -> i32 {
+    input.iter().map(|s| lower_increment(s)).sum()
+}
+
+fn part2(input: &[Vec<i32>]) -> i32 {
+    input.iter().map(|s| front_lower_increment(s)).sum()
+}
+
+fn main() {
+    let args = Cli::parse();
+    let input = std::fs::read_to_string(args.path).unwrap();
+    let (_, sequences) = sequences(&input).unwrap();
+    println!("Part 1: {}", part1(&sequences));
+    println!("Part 2: {}", part2(&sequences));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_single_line_parse() {
+        let input = "0 3 6 9 12 15";
+        let (_, output) = super::sequences(input).unwrap();
+        assert_eq!(output, vec![vec![0, 3, 6, 9, 12, 15]])
+    }
+
+    #[test]
+    fn test_single_line_parse_with_newline() {
+        let input = "0 3 6 9 12 15\n";
+        let (_, output) = super::sequences(input).unwrap();
+        assert_eq!(output, vec![vec![0, 3, 6, 9, 12, 15]])
+    }
+
+    #[test]
+    fn test_multiline_parse() {
+        let input = "0 3 6 9 12 15\n0 3 6 9 12 15\n";
+        let (_, output) = super::sequences(input).unwrap();
+        assert_eq!(
+            output,
+            vec![vec![0, 3, 6, 9, 12, 15], vec![0, 3, 6, 9, 12, 15]]
+        )
+    }
+
+    #[test]
+    fn test_example_single() {
+        let input = [0, 3, 6, 9, 12, 15];
+        assert_eq!(lower_increment(&input), 18)
+    }
+
+    #[test]
+    fn test_full_example() {
+        let input = "0 3 6 9 12 15\n1 3 6 10 15 21\n10 13 16 21 30 45";
+        let (_, output) = sequences(input).unwrap();
+        println!("{:?}", output);
+        assert_eq!(part1(&output), 114)
+    }
+
+    #[test]
+    fn test_front_lower_increment() {
+        let input = [10, 13, 16, 21, 30, 45];
+
+        assert_eq!(front_lower_increment(&input), 5)
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let input = "0 3 6 9 12 15\n1 3 6 10 15 21\n10 13 16 21 30 45";
+        let (_, output) = sequences(input).unwrap();
+        assert_eq!(part2(&output), 2)
+    }
+}
