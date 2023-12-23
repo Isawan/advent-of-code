4a84b0c44e86bca0883ed7e2cb3d4d40eb1944a0
Isawan Millican <admin@isawan.net>
2023-12-23 21:41:16 +0000

    day14 part 2 Correct algorithm but too slow

--- 2023/src/bin/day14.rs
+++ 2023/src/bin/day14.rs
@@ -1,3 +1,5 @@
+use std::cell::RefCell;
+
 use clap::Parser;
 
 #[derive(Parser, Debug, Clone)]
@@ -14,9 +16,9 @@ enum Item {
     Empty,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 struct Grid {
-    items: Vec<Vec<Item>>,
+    items: Vec<Item>,
     width: usize,
     height: usize,
 }
@@ -25,6 +27,7 @@ impl Grid {
     fn new(items: Vec<Vec<Item>>) -> Self {
         let height = items.len();
         let width = items[0].len();
+        let items = items.into_iter().flat_map(|x| x.into_iter()).collect();
         Self {
             items,
             width,
@@ -36,7 +39,45 @@ impl Grid {
         if x >= self.width || y >= self.height {
             return None;
         }
-        Some(self.items[y][x])
+        Some(self.items[y * self.width + x])
+    }
+
+    fn update(self, new_rounds: &mut Vec<(usize, usize)>) -> Self {
+        let mut items = self.items;
+        // reset grid
+        for item in items.iter_mut() {
+            *item = match item {
+                Item::Round => Item::Empty,
+                Item::Cube => Item::Cube,
+                Item::Empty => Item::Empty,
+            };
+        }
+        // update grid
+        for (x, y) in new_rounds {
+            items[*y * self.width + *x] = Item::Round;
+        }
+        Self {
+            items,
+            width: self.width,
+            height: self.height,
+        }
+    }
+}
+
+impl std::fmt::Debug for Grid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        //for self.items
+        //for line in self.items.iter() {
+        //    for item in line.iter() {
+        //        match item {
+        //            Item::Round => write!(f, "O")?,
+        //            Item::Cube => write!(f, "#")?,
+        //            Item::Empty => write!(f, ".")?,
+        //        }
+        //    }
+        //    writeln!(f)?;
+        //}
+        Ok(())
     }
 }
 
@@ -58,59 +99,130 @@ fn grid(input: &str) -> Grid {
     )
 }
 
-fn new_round_positions(grid: &Grid) -> Vec<(usize, usize)> {
-    (0..grid.width)
-        .flat_map(|x| {
-            (0..grid.height)
-                .scan(0, move |highest, y| {
-                    let item = grid.get(x, y).unwrap();
-                    match item {
-                        Item::Empty => Some(None),
-                        Item::Round => {
-                            *highest = *highest + 1;
-                            Some(Some((x, *highest - 1)))
-                        }
-                        Item::Cube => {
-                            *highest = y + 1;
-                            Some(None)
-                        }
-                    }
-                })
-                .filter_map(|x| x)
+fn new_round_positions(visits: impl Iterator<Item = (Item)>) -> impl Iterator<Item = usize> {
+    visits
+        .enumerate()
+        .scan(0, move |highest, (w, item)| match item {
+            Item::Empty => Some(None),
+            Item::Round => {
+                *highest += 1;
+                Some(Some(*highest - 1))
+            }
+            Item::Cube => {
+                *highest = w + 1;
+                Some(None)
+            }
         })
-        .collect()
+        .flatten()
 }
 
-fn part1(grid: &Grid) -> usize {
-    new_round_positions(grid)
-        .iter()
+fn calc_load(grid: &Grid) -> usize {
+    (0..grid.width)
+        .flat_map(|x| {
+            new_round_positions((0..grid.height).map(move |y| grid.get(x, y).unwrap()))
+                .map(move |y| (x, y))
+        })
         .map(|(_, y)| grid.height - y)
+        .sum()
+}
+
+thread_local! {
+    static BUF: RefCell<Vec<(usize, usize)>> = RefCell::new(Vec::new());
+}
+
+fn cycle(mut grid: Grid) -> Grid {
+    BUF.with(|buf| {
+        let buf = &mut *buf.borrow_mut();
+        grid = {
+            let grid_ref = &grid;
+            buf.extend((0..grid.width).flat_map(|x| {
+                new_round_positions((0..grid.height).map(move |y| grid_ref.get(x, y).unwrap()))
+                    .map(move |y| (x, y))
+            }));
+            grid.update(buf)
+        };
+        buf.clear();
+        grid = {
+            let grid_ref = &grid;
+            buf.extend((0..grid.height).flat_map(|y| {
+                new_round_positions((0..grid.width).map(move |x| grid_ref.get(x, y).unwrap()))
+                    .map(move |x| (x, y))
+            }));
+            grid.update(buf)
+        };
+        buf.clear();
+        grid = {
+            let grid_ref = &grid;
+            buf.extend((0..grid.width).flat_map(|x| {
+                new_round_positions(
+                    ((0..grid.height).rev()).map(move |y| grid_ref.get(x, y).unwrap()),
+                )
+                .map(move |y| (x, grid_ref.height - y - 1))
+            }));
+            grid.update(buf)
+        };
+        buf.clear();
+        grid = {
+            let grid_ref = &grid;
+            buf.extend((0..grid.height).flat_map(|y| {
+                new_round_positions(
+                    ((0..grid.width).rev()).map(move |x| grid_ref.get(x, y).unwrap()),
+                )
+                .map(move |x| (grid_ref.width - x - 1, y))
+            }));
+            grid.update(buf)
+        };
+        buf.clear();
+        grid
+    })
+}
+
+fn part2(input: Grid) -> usize {
+    let mut grid = input;
+    for i in 0..1_000_000 {
+        if i % 100_000 == 0 {
+            println!("{}", i);
+        }
+        grid = cycle(grid);
+    }
+    let grid_ref = &grid;
+    (0..grid.width)
+        .flat_map(|x| (0..grid.height).map(move |y| (x, y, grid_ref.get(x, y).unwrap())))
+        .filter(|(_, _, item)| *item == Item::Round)
+        .map(|(_, y, _)| grid.height - y)
         .sum()
 }
 
 fn main() {
     let args = Cli::parse();
     let input = std::fs::read_to_string(args.path).unwrap();
-    println!("Part 1: {}", part1(&grid(&input)));
+    println!("Part 1: {}", calc_load(&grid(&input)));
+    println!("Part 2: {}", part2(grid(&input)));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    //    #[test]
-    //    fn test_new_positions() {
-    //        let input = include_str!("../../input/day14-example");
-    //        let grid = grid(input);
-    //        assert_eq!(
-    //            new_round_positions,
-    //        )
-    //    }
-
     #[test]
     fn test_example() {
         let input = include_str!("../../input/day14-example");
         let grid = grid(input);
-        assert_eq!(part1(&grid), 136);
+        assert_eq!(calc_load(&grid), 136);
+    }
+
+    #[test]
+    fn test_example_after_one_cycle() {
+        let input = include_str!("../../input/day14-example");
+        let one_cycle = grid(include_str!("../../input/day14-example-one-cycle"));
+        let grid = grid(input);
+        assert_eq!(cycle(grid), one_cycle);
+    }
+
+    #[test]
+    fn test_example_part2() {
+        let input = include_str!("../../input/day14-example");
+        let grid = grid(input);
+        assert_eq!(part2(grid), 64);
     }
 }
